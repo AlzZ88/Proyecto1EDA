2b88f173a19c7b61122a9448dcba41c6929c16f8
MilenkoBonacich <44931596+MilenkoBonacich@users.noreply.github.com>
2021-09-20 17:29:53 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@ using namespace std;
 int main(){
     //statements
     BinaryHeap *bh=new BinaryHeap();
+    BinaryHeap *bb=new BinaryHeap();
     int rep=100;
-    for (int i = 0; i < 10; i++){
+    for (int i = 10; i < 20; i++){
         bh->Insert(i);
+        cout<<"i:"<<i<<endl;
+    }
+    for (int i = 0; i < 10; i++){
+        bb->Insert(i);
         cout<<"i:"<<i<<endl;
     }
     bh->show();
@@ -15,6 +20,9 @@ int main(){
     for (int i = 0; i < rep; i++){
         //bh->Insert(i);
     }
+    if(bh->Search(13))cout<<"Yes"<<endl;
+    bh->Union(*bb);
+    bh->show();
     double duracion= (double)((double) (clock()-start)/CLOCKS_PER_SEC/rep);
     cout<<"Insert;"<<duracion<<endl;
     return 0;
